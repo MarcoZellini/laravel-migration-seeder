323130263805ab77600a5329a92053e37e7e5207
Marco Zellini <zellinimarco@gmail.com>
2023-10-30 15:48:52 +0100

    Add Page Controller, Routes and Page Structuring

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,39 @@
+<footer class="bg-dark text-white py-2">
+    <div id="footer_top" class="py-3">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-md-3 text-center">
+                <div class="col">
+                    <ul class="list-unstyled">
+                        <li><a href="" class="text-decoration-none text-white">Lorem.</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Atque.</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Pariatur!</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Reprehenderit?</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="list-unstyled">
+                        <li><a href="" class="text-decoration-none text-white">Lorem.</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Atque.</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Pariatur!</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Reprehenderit?</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <ul class="list-unstyled">
+                        <li><a href="" class="text-decoration-none text-white">Lorem.</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Atque.</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Pariatur!</a></li>
+                        <li><a href="" class="text-decoration-none text-white">Reprehenderit?</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /#footer_top -->
+    <div id="footer_bottom">
+        <div class="container">
+            <h6 class="text-center m-0">2023 Zellini Marco</h6>
+        </div>
+    </div>
+    <!-- /#footer_bottom -->
+</footer>
